7999c5bbcd212b242ec8986df33e7732dd65b0d3
commey180 <com23005@byui.edu>
2024-09-22 12:27:36 +0000

    hello world file added

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+'''Title: Hello World Software
+   Author: Nicholas Oblitey Commey
+   Purpose: Meet Week 1 Assignment for Applied Programming
+   Date: 22nd September, 2024'''
+   
+   
+print("Hello World!")    
